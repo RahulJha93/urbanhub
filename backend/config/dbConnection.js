3f8020562b02f933c207789075555afe1fd85e9c
Rahul Jha <rahul.sahyogcollege@gmail.com>
2024-07-12 03:30:53 +0530

    done

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     let DB_URL;
-    // let DB_URI="mongodb+srv://rahulsahyog:roVimHbxCjibWBsQ@cluster0.rozqtvw.mongodb.net/?retryWrites=true&w=majority";
-    if(process.env.NODE_ENV==="DEVELOPMENT"){DB_URL=process.env.DB_LOCAL_URL;}
+    if(process.env.NODE_ENV==="DEVELOPMENT"){DB_URL=process.env.DB_URL;}
     if(process.env.NODE_ENV==="PRODUCTION"){DB_URL=process.env.DB_URL;}
 
     mongoose.connect(DB_URL)
