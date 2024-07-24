331d3e086cff3ce2bc1d66415384e06f3a81b3d8
Rahul Jha <rahul.sahyogcollege@gmail.com>
2024-07-24 15:51:59 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -48,13 +48,14 @@ app.use(
   })
 );
 const options = {
-  expires: new Date(
-    Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-  ),
+  // expires: new Date(
+  //   Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+  // ),
   httpOnly: true,
   secure: true, // Set to true if your using https
   sameSite: 'None', // Allows cross-site cookies
-  credentials:"include"
+  domain:"https://urbanhub.vercel.app",
+  // credentials:"include"
 
 };
 app.use(cookieParser(options));
