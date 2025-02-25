138328cff9c6475786af03fa9491cb7a8b5b8fa8
Rahul Jha <rahul.sahyogcollege@gmail.com>
2025-02-26 03:42:56 +0530

    fix : api not working

--- frontend/src/redux/api/orderApi.js
+++ frontend/src/redux/api/orderApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const orderApi = createApi({
   reducerPath: "orderApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
+    baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1`, // base url
     headers: {
       "Content-Type": "application/json",
     },
